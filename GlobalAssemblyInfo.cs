2dbaca32e0a617044b6832d4eec1ba6a7a599f00
michael dudley <michael.dudley@improvingenterprises.com>
2017-03-27 11:45:33 -0500

    updating nuspec

--- GlobalAssemblyInfo.cs
+++ GlobalAssemblyInfo.cs
@@ -1,11 +1,11 @@
 ﻿using System.Reflection;
 using System.Resources;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyCompany("Six Flags Entertainment, Inc")]
-[assembly: AssemblyCopyright("Copyright © 2016")]
+[assembly: AssemblyCompany("Improving")]
+[assembly: AssemblyCopyright("Copyright © 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en-US")]
--- Improving.AspNet/Properties/AssemblyInfo.cs
+++ Improving.AspNet/Properties/AssemblyInfo.cs
@@ -5,32 +5,15 @@ using System.Runtime.InteropServices;
 [assembly: NRConfig.Instrument]
 #endif
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("Improving.AspNet")]
-[assembly: AssemblyDescription("Six Flags Web Integration")]
+[assembly: AssemblyDescription("Improving web api, mvc, and mediatr integration")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyProduct("Improving.AspNet")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("21ddefde-89b8-4c8c-a150-6cdd527a758c")]
+[assembly: Guid("DD6E5273-2FC8-40CF-BC90-B83542C50EA8")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion('1.0.*')]
 [assembly: AssemblyVersion("0.0.0.0")] //updated in TeamCity
 [assembly: AssemblyFileVersion("0.0.0.0")] //updated in TeamCity
 [assembly: AssemblyInformationalVersion("0.0.0.0")] //updated in TeamCity
